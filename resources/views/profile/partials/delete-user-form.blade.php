d88768f0867aeead58f436a5853acb1848a1a770
borquezln <borquezln@hotmail.com>
2026-06-15 05:17:41 -0300

    fix(ui): dark mode on profile (v1.2.1)

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,10 +1,10 @@
 <section class="space-y-6">
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             Eliminar cuenta
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             Si eliminás tu cuenta, todos sus datos se borrarán de forma permanente.
         </p>
     </header>
@@ -19,11 +19,11 @@
             @csrf
             @method('delete')
 
-            <h2 class="text-lg font-medium text-gray-900">
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
                 ¿Seguro que querés eliminar tu cuenta?
             </h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                 Esta acción es permanente. Ingresá tu contraseña para confirmar.
             </p>
 
